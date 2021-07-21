73a3e8e0b8825796cdcda61f029a904ef92673a7
Elber Castillo Mesa <ecastillo@pros.com>
2021-07-20 22:18:57 -0500

    Cleanup react default content and create the components folder, Navbar component created without logic, also the Dashboard component was created with dummy data.

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const ProjectList = () => {
+    return(
+        <div className="project-list section">
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by EPCM</p>
+                    <p className="grey-text">Today 10PM</p>
+                </div>
+            </div>
+
+            <div className="card z-depth-0 project-summary">
+                <div className="card-content grey-text text-darken-3">
+                    <span className="card-title">Project Title</span>
+                    <p>Posted by EPCM</p>
+                    <p className="grey-text">Today 10PM</p>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default ProjectList;
